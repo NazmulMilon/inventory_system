58740904372333c779c27877b3f9ff1ae165fad5
Md. Milon Sarker <milon16103373@gmail.com>
2021-08-27 01:38:20 +0600

    update inventory

--- inventoryproject/inventoryproject/urls.py
+++ inventoryproject/inventoryproject/urls.py
@@ -13,18 +13,33 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from inventoryproject.settings import MEDIA_ROOT
 from django.contrib import admin
 from django.urls import path, include
 from user import views as user_view
 from django.contrib.auth import views as auth_views
+#from django.conf import settings
+#from django.conf.urls.static import static
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('dashboard.urls')),
     path('register/', user_view.register, name='user-register'),
     path('profile/', user_view.profile, name='user-profile'),
+    path('profile/update', user_view.profile_update, name='user-profile-update'),
     path('', auth_views.LoginView.as_view(template_name='user/login.html'), name='user-login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='user-logout'),
 
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+#urlpatterns = [
+
+#]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
